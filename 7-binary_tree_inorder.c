b031294390e88ab1360b4d9597a9e71876effaa8
Holic65 <okormark@gmail.com>
2022-09-22 16:29:35 -0700

    fixing

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_order - a function that performs inorder traversal on a tree
- * @tree: root node of the tree
- * @func: function to print value of node
+ * binary_tree_inorder - a function that uses inorder traversal on a tree
+ * @tree: root node
+ * @func: a function to read value of node
  *
- * Return: Nothing
  */
 
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
 {
 	if (!tree || !func)
 		return;
