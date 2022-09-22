27bf5dd0bb09f77e94ed134d48d28c9835401f6f
Holic65 <okormark@gmail.com>
2022-09-22 16:22:27 -0700

    trying

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#inclued "binary_trees.h"
+/**
+ * binary_tree_postorder - a function that uses postorder traversal on a tree
+ * @tree: root node
+ * @func: a function to read value of node
+ *
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void(*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
