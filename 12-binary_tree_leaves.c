46c6a02921d202473da806ae41da638560a37c9a
Holic65 <okormark@gmail.com>
2022-09-23 16:52:23 -0700

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,44 +1,21 @@
 #include "binary_trees.h"
-
-void traverse(const binary_tree_t *tree, size_t *count);
-
 /**
- * binary_tree_leaves - get the number of leaves in a binary tree
- * @tree: tree to check
+ * binary_tree_leaves - a function that counts leaves on a tree
+ * @tree: the tree to be checked
  *
- * Return: size of tree
+ * Return: number of leaves in a tree or NULL if otherwise
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t leaves = 0;
 
-	if (!tree)
-		return (count);
-	traverse(tree, &count);
-	return (count);
-}
+	if (tree == NULL)
+		return (0);
 
-
-/**
- * traverse - goes through a binary tree using in-order traversal
- * @tree: a pointer to the root node of the tree to traverse
- * @count: current count of tree leaves during traversal
- */
-
-void traverse(const binary_tree_t *tree, size_t *count)
-{
 	if (!tree->left && !tree->right)
-	{
-		*count += 1;
-		return;
-	}
-	if (tree->left)
-	{
-		traverse(tree->left, count);
-	}
-	if (tree->right)
-	{
-		traverse(tree->right, count);
-	}
+		return (leaves + 1);
+
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
